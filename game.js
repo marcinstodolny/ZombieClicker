151b65667e673c4bf2bf67f85fc40cab4d3db134
Ostin-Oskarose <oskarcraft@gmail.com>
2023-01-09 13:18:55 +0100

    Base game

--- game.js
+++ game.js
@@ -1,7 +1,18 @@
+let score = 1;
 initGame();
 
 function initGame() {
 
     // Your game can start here, but define separate functions, don't write everything in here :)
+    updateScore();
 
+}
+
+function updateScore() {
+    document.getElementById('score').innerText = score + ' zombies';
+}
+
+function buttonClick() {
+    score++;
+    updateScore();
 }
