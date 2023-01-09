cb29050d80590b300a16b4d7b4a84d0da4207b01
Marcin Stodolny <marcin.stodolny@o2.pl>
2023-01-09 21:07:37 +0100

    saved stats to cookies, increased price by 1.5 and reset button to better check game

--- game.js
+++ game.js
@@ -8,11 +8,25 @@ let totalPopulation = 8010096000;
 initGame();
 
 function initGame() {
-
+    if ((Number(document.cookie.match(new RegExp('(^| )' + 'brains' + '=([^;]+)'))) !== 0))
+        readCookies()
     // Your game can start here, but define separate functions, don't write everything in here :)
-    updateStats();
+    updateGame();
     setInterval(idle_loop, 1000);
+    setInterval(updateGame, 100);
 
+
+}
+function readCookies(){
+    zombies = Number(document.cookie.match(new RegExp('(^| )' + 'zombies' + '=([^;]+)'))[2]);
+    brains = Number(document.cookie.match(new RegExp('(^| )' + 'brains' + '=([^;]+)'))[2]);
+    buildings = JSON.parse(document.cookie.match(new RegExp('(^| )' + 'buildings' + '=([^;]+)'))[2]);
+    clickPower = Number(document.cookie.match(new RegExp('(^| )' + 'clickPower' + '=([^;]+)'))[2]);
+    clicksPerSecond = Number(document.cookie.match(new RegExp('(^| )' + 'clicksPerSecond' + '=([^;]+)'))[2]);
+    for (let i = 0; i< buildings.length; i++){
+        document.getElementById(buildings[i]['name'].toLowerCase()).value = buildings[i]['current_price'];
+        document.getElementById(buildings[i]['name'].toLowerCase()).innerText = buildings[i]['name'] + ' '+ buildings[i]['current_price'] + ' brains';
+    }
 }
 
 function idle_loop() {
@@ -21,16 +35,22 @@ function idle_loop() {
     zombies = Math.min(zombies, totalPopulation);
     let zombieDelta = zombies - initialZombies;
     brains += zombieDelta;
-    updateStats();
+    updateGame();
 }
 
-function updateStats() {
+function updateGame() {
     document.getElementById('brains').innerText = brains + ' brains';
     document.getElementById('living').innerText = totalPopulation - zombies + ' living';
     document.getElementById('zombies').innerText = zombies + ' zombies';
+    document.cookie = 'brains=' + brains + '; expires=Thu, 18 Dec 2033 12:00:00 UTC"';
+    document.cookie = 'zombies=' + zombies + '; expires=Thu, 18 Dec 2033 12:00:00 UTC"';
+    document.cookie = 'buildings=' + JSON.stringify(buildings) + '; expires=Thu, 18 Dec 2033 12:00:00 UTC"';
+    document.cookie = 'clicksPerSecond=' + clicksPerSecond + '; expires=Thu, 18 Dec 2033 12:00:00 UTC"';
+    document.cookie = 'clickPower=' + clickPower + '; expires=Thu, 18 Dec 2033 12:00:00 UTC"';
     if (zombies === totalPopulation) {
         alert('You win!')
     }
+    getBuildings();
 }
 
 function buttonClick() {
@@ -39,20 +59,40 @@ function buttonClick() {
     zombies = Math.min(zombies, totalPopulation);
     let zombieDelta = zombies - initialZombies;
     brains += zombieDelta;
-    updateStats();
+    updateGame();
 }
 function buyElement(element, price, cps){
+    let element_name = element.charAt(0).toUpperCase() + element.slice(1)
     if (price <= brains){
-        if (buildings.some(item => item.name === element)){
-            let index = buildings.findIndex(({ name }) => name === element);
+        let new_price = Math.round(price * 1.5)
+        if (buildings.some(item => item.name === element_name)){
+            let index = buildings.findIndex(({ name }) => name === element_name);
             buildings[index]['count'] += 1;
-            brains -= price;
-            clicksPerSecond += cps;
+            buildings[index]['current_price'] = new_price;
         } else {
-            buildings[buildings.length] = {name:element, count:1};
-            brains -= price;
-            clicksPerSecond += cps;
+            buildings[buildings.length] = {name:element_name, count:1, current_price:new_price};
         }
+        brains -= price;
+        clicksPerSecond += cps;
+
+        document.getElementById(element).innerText = element_name + ' '+ new_price + ' brains';
+        document.getElementById(element).value = new_price;
     }
-    updateStats();
+    updateGame();
+}
+function getBuildings(){
+    let text = ''
+    for (let i = 0; i< buildings.length; i++){
+        text += (buildings[i]['name'] +' '+ buildings[i]['count'] + '\n')
+        }
+    document.getElementById('buildings').innerText = text
+}
+function resetGame() {
+    brains = 0;
+    zombies = 0;
+    clickPower = 1;
+    clicksPerSecond = 0;
+    buildings = [];
+    totalPopulation = 8010096000;
+    updateGame()
 }
