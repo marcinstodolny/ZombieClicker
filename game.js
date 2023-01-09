2da920e20251dc2d3d77df0bf747319bf5815a2f
Ostin-Oskarose <oskarcraft@gmail.com>
2023-01-09 14:00:40 +0100

    Added win condition

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ let brains = 0;
 let zombies = 0;
 let clickPower = 1;
 let clicksPerSecond = 0;
+let totalPopulation = 8010096000;
 initGame();
 
 function initGame() {
@@ -13,18 +14,28 @@ function initGame() {
 }
 
 function idle_loop() {
-        brains += clicksPerSecond;
-        zombies += clicksPerSecond;
-        updateStats();
+    let initialZombies = zombies;
+    zombies += clicksPerSecond;
+    zombies = Math.min(zombies, totalPopulation);
+    let zombieDelta = zombies - initialZombies;
+    brains += zombieDelta;
+    updateStats();
 }
 
 function updateStats() {
     document.getElementById('brains').innerText = brains + ' brains';
+    document.getElementById('living').innerText = totalPopulation - zombies + ' living';
     document.getElementById('zombies').innerText = zombies + ' zombies';
+    if (zombies === totalPopulation) {
+        alert('You win!')
+    }
 }
 
 function buttonClick() {
-    brains += clickPower;
+    let initialZombies = zombies;
     zombies += clickPower;
+    zombies = Math.min(zombies, totalPopulation);
+    let zombieDelta = zombies - initialZombies;
+    brains += zombieDelta;
     updateStats();
 }
