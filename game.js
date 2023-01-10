31cfb07ef606c5c9a38e5b6d84e006779b6d79a0
Marcin Stodolny <marcin.stodolny@o2.pl>
2023-01-10 12:06:41 +0100

    some changes

--- game.js
+++ game.js
@@ -5,6 +5,8 @@ let clickPowerMultiplier = 1;
 let clicksPerSecond = 0;
 let clicksPerSecondMultiplier = 1;
 let buildings = [];
+let upgrades = [{name:'shovel', price:100, buildingName:'Cemetery', requirement:5}];
+let bought_upgrades = [];
 let totalPopulation = 8010096000;
 
 initGame();
@@ -22,6 +24,7 @@ function readCookies(){
     zombies = Number(document.cookie.match(new RegExp('(^| )' + 'zombies' + '=([^;]+)'))[2]);
     brains = Number(document.cookie.match(new RegExp('(^| )' + 'brains' + '=([^;]+)'))[2]);
     buildings = JSON.parse(document.cookie.match(new RegExp('(^| )' + 'buildings' + '=([^;]+)'))[2]);
+    bought_upgrades = JSON.parse(document.cookie.match(new RegExp('(^| )' + 'bought_upgrades' + '=([^;]+)'))[2]);
     clickPower = Number(document.cookie.match(new RegExp('(^| )' + 'clickPower' + '=([^;]+)'))[2]);
     clicksPerSecond = Number(document.cookie.match(new RegExp('(^| )' + 'clicksPerSecond' + '=([^;]+)'))[2]);
     for (let i = 0; i< buildings.length; i++){
@@ -46,9 +49,11 @@ function updateGame() {
     document.cookie = 'brains=' + brains + '; expires=Thu, 18 Dec 2033 12:00:00 UTC"';
     document.cookie = 'zombies=' + zombies + '; expires=Thu, 18 Dec 2033 12:00:00 UTC"';
     document.cookie = 'buildings=' + JSON.stringify(buildings) + '; expires=Thu, 18 Dec 2033 12:00:00 UTC"';
+    document.cookie = 'bought_upgrades=' + JSON.stringify(bought_upgrades) + '; expires=Thu, 18 Dec 2033 12:00:00 UTC"';
     document.cookie = 'clicksPerSecond=' + clicksPerSecond + '; expires=Thu, 18 Dec 2033 12:00:00 UTC"';
     document.cookie = 'clickPower=' + clickPower + '; expires=Thu, 18 Dec 2033 12:00:00 UTC"';
     updateStatistics();
+    showUpgrades();
     if (zombies === totalPopulation) {
         alert('You win!')
     }
@@ -89,6 +94,27 @@ function buyElement(element, price, cps){
     }
     updateGame();
 }
+function buyUpgrade(element, price, cp){
+     if (price <= brains){
+         bought_upgrades.push(element)
+         brains -= price
+         clickPower += cp
+     }
+}
+function showUpgrades(){
+    let text = ''
+    if (buildings.length !== 0){
+        for(let i = 0; i < upgrades.length; i++){
+            let index = buildings.findIndex(({ name }) => name === upgrades[i]['buildingName']);
+            if (upgrades[i]['requirement'] <= buildings[index]['count'] && !bought_upgrades.some(item => item.name === upgrades[i]['name'])){
+                text += '<button id="' + upgrades[i]['name'] + '" value="10">' + building['name'] + ' ' + building['cost'] + ' brains</button>';
+                text += "<button id=" + upgrades[i]['name'] + 'value=\'100\' onClick="buyUpgrade(\'shovel\', Number(this.value), 1)">Shovel 100 brains</button>'
+        }
+    }
+    }
+
+    document.getElementById('upgrades').innerHTML = text
+}
 
 function getBuildings(){
     let text = ''
@@ -104,6 +130,7 @@ function resetGame() {
     clickPower = 1;
     clicksPerSecond = 0;
     buildings = [];
+    bought_upgrades = [];
     totalPopulation = 8010096000;
     location.reload()
     updateGame()
